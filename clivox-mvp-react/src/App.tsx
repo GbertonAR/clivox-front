625f15ee49703507427165d9f8b9126e55fb44b1
GBertonAR <gberton1967@gmail.com>
2025-07-04 15:25:44 -0300

    avance fase1

--- clivox-mvp-react/src/App.tsx
+++ clivox-mvp-react/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Instructor from './pages/Instructor'
 import Client from './pages/client'
 import Dashboard from './pages/Dashboard'
-import home from './pages/home'
+import Home from './pages/home'
 
 function App() {
   return (
